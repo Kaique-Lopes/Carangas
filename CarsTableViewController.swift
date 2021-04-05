2f701863263da5118f5ba787dfbc8f60a8eec22c
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-05 19:10:53 -0300

    Rest Load Cars

--- CarsTableViewController.swift
+++ CarsTableViewController.swift
@@ -1,20 +1,16 @@
 import UIKit
 
 class CarsTableViewController: UITableViewController {
-
+    var cars: [Car] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        REST.loadCars()
     }
 
     // MARK: - Table view data source
--- REST.swift
+++ REST.swift
@@ -15,12 +15,40 @@ class REST {
         let config = URLSessionConfiguration.default
         config.allowsCellularAccess = false
         config.httpAdditionalHeaders = ["Content-type": "application/json"]
-        config.timeoutIntervalForRequest = 30.0
         config.httpMaximumConnectionsPerHost = 5
         return config
     } ()
     
     private static let session = URLSession(configuration: configuration)
     
+    // MARK: - Method Class
+    class func loadCars() {
+        guard let url = URL(string: basePath) else {return}
+        
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                
+                guard let response = response as? HTTPURLResponse else {return}
+                if response.statusCode == 200 {
+                    
+                    guard let data = data else {return}
+                    do {
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        for car in cars {
+                            print(car.name, car.brand)
+                        }
+                    }catch {
+                        print(error.localizedDescription)
+                    }
+                }else {
+                    print("Algum Status inválido pelo servidor!!")
+                }
+                
+            }else {
+                print(error!)
+            }
+        }
+        dataTask.resume()
+    }
     
 }
